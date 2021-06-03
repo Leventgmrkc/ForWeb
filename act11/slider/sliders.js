0fcac50dc1dfd8a6338c4f7dba9d26052792b453
Leventgmrkc <66283966+Leventgmrkc@users.noreply.github.com>
2021-06-04 02:02:00 +0300

    Add files via upload

---
+++ act11/slider/sliders.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+        	$("#slider").bxSlider({
+			  auto: true,
+			  minSlides: 1,
+			  maxSlides: 1,
+			  slideWidth: 250,
+			  slideMargin: 10,
+			  randomStart: true,
+			  speed: 3000,
+			  pager: true,
+			  pagerType: 'short',
+			  pagerSelector : '#pager',
+			  moveslides:1
+			});
+    	});
